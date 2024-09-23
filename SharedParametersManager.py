80d6b40b6e4d4c5e11130802efb2c4ffbdf64a39
Vico <tringola@users.noreply.github.com>
2024-09-23 15:50:37 +0200

    Update SharedParametersManager.py
    Cette class va changer de nom car c'est une classe qui va gerer toutes les parametres ajoutez aux familles et aux projets et gabarits

--- SharedParametersManager.py
+++ SharedParametersManager.py
@@ -13,18 +13,44 @@ class SharedParametersManager:
     #si chemin du fichier est donné il ouvre le fichier et change, sinon il change le fichier ouvert
     def createGroupsInSharedParameters(self,name,path='null'):       
         originalFile = self.app.SharedParametersFilename
-        #app.SharedParametersFilename = tempFile
+        #si le path (complet) du fichier est donné, donc prendre ce fichier comme base
+        if path != 'null':
+            app.SharedParametersFilename = path
         sharedParameterFile = self.app.OpenSharedParameterFile()
         #txt group name 
         GroupName = sharedParameterFile.Groups.Create(name)
         return GroupName
         
-    def createSharedParameterInGroup(self,name):
+    def createType :
+        
+    def createParameterFromShared(self,parameterName,group,typeOfFamily,IsInstance):
+        
         originalFile = self.app.SharedParametersFilename
         #app.SharedParametersFilename = tempFile
         sharedParameterFile = self.app.OpenSharedParameterFile()
         #txt parametres name 
+        doc.FamilyManager.AddParameter()
         return 
     
-    def addParameterToPoject(self,name,file,groupe):
+    def addParameterToProject(self,name,file,groupe):
+    
     def createFileSharedParameters(self,path,):
+    
+    def getAllElementsOfCategory(self,Category):#il faut etre dans la forme BuiltInCategory.OST_Rooms par exemple
+    FilteredElements = FilteredElementCollector(doc) 
+    return FilteredElements.OfCategory(Category)
+
+
+    #Pour manipuler les objets trouvés il faut faire un iteration, car les elements de la liste ne sont  acessible par index
+    def setParameterByName(self,Element, parameter_name, value):
+        Element.LookupParameter(parameter_name).Set(value)
+        
+    def clearParameterByName(self,Element, parameter_name):
+        Element.LookupParameter(parameter_name).ClearValue()
+        
+    def getParameterAsString(self,Element, parameter_name):
+        return Element.LookupParameter(parameter_name).AsString()
+        
+    def search(self,param,param_value,list):
+        return [element for element in list if element[str(param)] == param_value]
+    
